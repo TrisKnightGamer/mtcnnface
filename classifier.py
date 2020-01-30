3f3f453f16ab2209c1494e9ef2998a36730e03d8
Hoàng Đức Trí <49811496+TrisKnightGamer@users.noreply.github.com>
2020-01-30 20:05:28 +0700

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -26,33 +26,56 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
-import numpy as np
-import argparse
 import facenet
-import os
-import sys
 import math
+import argparse
+import logging
+import os
 import pickle
-from sklearn.svm import SVC
+import sys
+import time
 
-def main(args):
-  
-    with tf.Graph().as_default():
-      
-        with tf.Session() as sess:
-            
+import numpy as np
+import tensorflow as tf
+from sklearn.svm import SVC
+from tensorflow.python.platform import gfile
+
+from lfw_input import filter_dataset, split_dataset, get_dataset
+from medium_facenet_tutorial import lfw_input
+
+logger = logging.getLogger(__name__)
+
+def main(input_directory, model_path, classifier_output_path, batch_size, num_threads, num_epochs,
+         min_images_per_labels, split_ratio, is_train=True):
+
+    """
+    Loads images from :param input_dir, creates embeddings using a model defined at :param model_path, and trains
+     a classifier outputted to :param output_path
+     
+    :param input_directory: Path to directory containing pre-processed images
+    :param model_path: Path to protobuf graph file for facenet model
+    :param classifier_output_path: Path to write pickled classifier
+    :param batch_size: Batch size to create embeddings
+    :param num_threads: Number of threads to utilize for queuing
+    :param num_epochs: Number of epochs for each image
+    :param min_images_per_labels: Minimum number of images per class
+    :param split_ratio: Ratio to split train/test dataset
+    :param is_train: bool denoting if training or evaluate
+    """
+    start_time = time.time()
+    with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
             np.random.seed(seed=args.seed)
-            
-            if args.use_split_dataset:
-                dataset_tmp = facenet.get_dataset(args.data_dir)
-                train_set, test_set = split_dataset(dataset_tmp, args.min_nrof_images_per_class, args.nrof_train_images_per_class)
-                if (args.mode=='TRAIN'):
-                    dataset = train_set
-                elif (args.mode=='CLASSIFY'):
-                    dataset = test_set
+            train_set, test_set = _get_test_and_train_set(input_directory, min_num_images_per_label=min_images_per_labels,
+                                                split_ratio=split_ratio)
+            if is_train:
+                images, labels, class_names = _load_images_and_labels(train_set, image_size=160, batch_size=batch_size,
+                                                num_threads=num_threads, num_epochs=num_epochs,
+                                                random_flip=True, random_brightness=True,
+                                                random_contrast=True)
             else:
-                dataset = facenet.get_dataset(args.data_dir)
+                images, labels, class_names = _load_images_and_labels(test_set, image_size=160, batch_size=batch_size,
+                                                                  num_threads=num_threads, num_epochs=1)
+            _load_model(model_filepath=model_path)
 
             # Check that there are at least one training image per class
             for cls in dataset:
@@ -66,61 +89,34 @@ def main(args):
             
             # Load the model
             print('Loading feature extraction model')
-            facenet.load_model(args.model)
+            init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+            sess.run(init_op)
+
             
             # Get input and output tensors
             images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
-            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            embedding_layer = tf.get_default_graph().get_tensor_by_name("embeddings:0")
             phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-            embedding_size = embeddings.get_shape()[1]
             
             # Run forward pass to calculate embeddings
             print('Calculating features for images')
-            nrof_images = len(paths)
-            nrof_batches_per_epoch = int(math.ceil(1.0*nrof_images / args.batch_size))
-            emb_array = np.zeros((nrof_images, embedding_size))
-            for i in range(nrof_batches_per_epoch):
-                start_index = i*args.batch_size
-                end_index = min((i+1)*args.batch_size, nrof_images)
-                paths_batch = paths[start_index:end_index]
-                images = facenet.load_data(paths_batch, False, False, args.image_size)
-                feed_dict = { images_placeholder:images, phase_train_placeholder:False }
-                emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+
+            emb_array, label_array = _create_embeddings(embedding_layer, images, labels, images_placeholder,
+                                                    phase_train_placeholder, sess)
             
-            classifier_filename_exp = os.path.expanduser(args.classifier_filename)
+            coord.request_stop()
+            coord.join(threads=threads)
+            logger.info('Created {} embeddings'.format(len(emb_array)))
 
-            if (args.mode=='TRAIN'):
-                # Train classifier
-                print('Training classifier')
-                model = SVC(kernel='linear', probability=True)
-                model.fit(emb_array, labels)
-            
-                # Create a list of class names
-                class_names = [ cls.name.replace('_', ' ') for cls in dataset]
+            classifier_filename = classifier_output_path
+            if is_train:
+                _train_and_save_classifier(emb_array, label_array, class_names, classifier_filename)
+            else:
+                _evaluate_classifier(emb_array, label_array, classifier_filename)
 
-                # Saving classifier model
-                with open(classifier_filename_exp, 'wb') as outfile:
-                    pickle.dump((model, class_names), outfile)
-                print('Saved classifier model to file "%s"' % classifier_filename_exp)
-                
-            elif (args.mode=='CLASSIFY'):
-                # Classify images
-                print('Testing classifier')
-                with open(classifier_filename_exp, 'rb') as infile:
-                    (model, class_names) = pickle.load(infile)
-
-                print('Loaded classifier model from file "%s"' % classifier_filename_exp)
-
-                predictions = model.predict_proba(emb_array)
-                best_class_indices = np.argmax(predictions, axis=1)
-                best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
-                
-                for i in range(len(best_class_indices)):
-                    print('%4d  %s: %.3f' % (i, class_names[best_class_indices[i]], best_class_probabilities[i]))
-                    
-                accuracy = np.mean(np.equal(best_class_indices, labels))
-                print('Accuracy: %.3f' % accuracy)
-                
+            logger.info('Completed in {} seconds'.format(time.time() - start_time))
             
 def split_dataset(dataset, min_nrof_images_per_class, nrof_train_images_per_class):
     train_set = []
@@ -134,9 +130,105 @@ def split_dataset(dataset, min_nrof_images_per_class, nrof_train_images_per_clas
             test_set.append(facenet.ImageClass(cls.name, paths[nrof_train_images_per_class:]))
     return train_set, test_set
 
-            
+def _load_images_and_labels(dataset, image_size, batch_size, num_threads, num_epochs, random_flip=False,
+                            random_brightness=False, random_contrast=False):
+    class_names = [cls.name for cls in dataset]
+    image_paths, labels = lfw_input.get_image_paths_and_labels(dataset)
+    images, labels = lfw_input.read_data(image_paths, labels, image_size, batch_size, num_epochs, num_threads,
+                                         shuffle=False, random_flip=random_flip, random_brightness=random_brightness,
+                                         random_contrast=random_contrast)
+    return images, labels, class_names
+
+def _train_and_save_classifier(emb_array, label_array, class_names, classifier_filename_exp):
+    logger.info('Training Classifier')
+    model = SVC(kernel='linear', probability=True, verbose=False)
+    model.fit(emb_array, label_array)
+
+    with open(classifier_filename_exp, 'wb') as outfile:
+        pickle.dump((model, class_names), outfile)
+    logging.info('Saved classifier model to file "%s"' % classifier_filename_exp)
+
+def _get_test_and_train_set(input_dir, min_num_images_per_label, split_ratio=0.7):
+    """
+    Load train and test dataset. Classes with < :param min_num_images_per_label will be filtered out.
+    :param input_dir: 
+    :param min_num_images_per_label: 
+    :param split_ratio: 
+    :return: 
+    """
+    dataset = get_dataset(input_dir)
+    dataset = filter_dataset(dataset, min_images_per_label=min_num_images_per_label)
+    train_set, test_set = split_dataset(dataset, split_ratio=split_ratio)
+
+    return train_set, test_set
+
+def _create_embeddings(embedding_layer, images, labels, images_placeholder, phase_train_placeholder, sess):
+    """
+    Uses model to generate embeddings from :param images.
+    :param embedding_layer: 
+    :param images: 
+    :param labels: 
+    :param images_placeholder: 
+    :param phase_train_placeholder: 
+    :param sess: 
+    :return: (tuple): image embeddings and labels
+    """
+    emb_array = None
+    label_array = None
+    try:
+        i = 0
+        while True:
+            batch_images, batch_labels = sess.run([images, labels])
+            logger.info('Processing iteration {} batch of size: {}'.format(i, len(batch_labels)))
+            emb = sess.run(embedding_layer,
+                           feed_dict={images_placeholder: batch_images, phase_train_placeholder: False})
+
+            emb_array = np.concatenate([emb_array, emb]) if emb_array is not None else emb
+            label_array = np.concatenate([label_array, batch_labels]) if label_array is not None else batch_labels
+            i += 1
+
+    except tf.errors.OutOfRangeError:
+        pass
+
+    return emb_array, label_array
+
+def _evaluate_classifier(emb_array, label_array, classifier_filename):
+    logger.info('Evaluating classifier on {} images'.format(len(emb_array)))
+    if not os.path.exists(classifier_filename):
+        raise ValueError('Pickled classifier not found, have you trained first?')
+
+    with open(classifier_filename, 'rb') as f:
+        model, class_names = pickle.load(f)
+
+        predictions = model.predict_proba(emb_array, )
+        best_class_indices = np.argmax(predictions, axis=1)
+        best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
+
+        for i in range(len(best_class_indices)):
+            print('%4d  %s: %.3f' % (i, class_names[best_class_indices[i]], best_class_probabilities[i]))
+
+        accuracy = np.mean(np.equal(best_class_indices, label_array))
+        print('Accuracy: %.3f' % accuracy)
+
+def _load_model(model_filepath):
+    """
+    Load frozen protobuf graph
+    :param model_filepath: Path to protobuf graph
+    :type model_filepath: str
+    """
+    model_exp = os.path.expanduser(model_filepath)
+    if os.path.isfile(model_exp):
+        logging.info('Model filename: %s' % model_exp)
+        with gfile.FastGFile(model_exp, 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            tf.import_graph_def(graph_def, name='')
+    else:
+        logger.error('Missing model file. Exiting')
+        sys.exit(-1)
+
 def parse_arguments(argv):
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(add_help=True)
     
     parser.add_argument('mode', type=str, choices=['TRAIN', 'CLASSIFY'],
         help='Indicates if a new classifier should be trained or a classification ' + 
@@ -163,8 +255,8 @@ def parse_arguments(argv):
         help='Only include classes with at least this number of images in the dataset', default=20)
     parser.add_argument('--nrof_train_images_per_class', type=int,
         help='Use this number of images from each class for training and the rest for testing', default=10)
-    
-    return parser.parse_args(argv)
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
     main(parse_arguments(sys.argv[1:]))
